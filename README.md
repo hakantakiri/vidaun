948603346be51af73a1118651b9753f0aa8ad89a
Hakan <hakan@mbp18.com>
2023-11-02 19:39:08 -0500

    POC: Working POC

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ Universal media downloader
 ## Running app
 
 ```
-python3 app.py
+python3 app/__init__.py
+```
+
+## Building
+
+```
+    pyinstaller -w --onefile --name=vidaun app/__init__.py
 ```
---
+++ app/__init__.py
@@ -0,0 +1,102 @@
+import tkinter as tk
+from tkinter import filedialog
+from domain.services.video_service import download, get_info_without_download
+from pathvalidate import sanitize_filename
+
+def run() :
+
+    state_no_process = ''
+    state_loading = 'Loading'
+    state_finisçhed = 'Finished'
+    final_name = ''
+
+    root = tk.Tk()
+    root.title('Vidaun')
+
+
+
+
+    def getFolder():
+    
+        folder_selected = filedialog.askdirectory()
+        print('selected folder is:')
+        print(folder_selected)
+        return folder_selected
+
+
+
+    def click_download():
+        url.config(state = 'disabled')
+        print('Downloading url: {}'.format(url.get()))
+
+        folder_name = getFolder()
+        info = get_info_without_download(url.get())
+        
+        title = sanitize_filename(info['title'])
+        preview_url = info['thumbnails'][0]['url']
+        
+        formats = []
+        for f in info['formats']:
+            formats.append({
+                'format': f['format_id'], 
+                'resolution': f['resolution'],
+                'ext':f['ext']
+                })
+
+        print('info:')
+        print(info['formats'])
+        # print(info['title'])
+        # print(formats)
+        print('preview url: ', preview_url)
+
+
+
+        url.config(state='disabled')
+        download_button.config(state='disabled')
+        name.config(state='normal')
+
+        name.insert(0, folder_name+'/'+title.replace(' ', '_')+'.mp4')
+
+        save_button.config(state='normal')
+
+
+
+    def save():
+
+        name.config(state='disabled')
+        download(url.get(), name.get())
+
+        url.config(state='normal')
+        download_button.config(state='normal')
+
+        name.config(state='disabled')
+        save_button.config(state='disabled')
+
+
+    title_label = tk.Label(root, text = "Vidaun")
+    input_url_label = tk.Label(root, text = "Insert Url")
+    url = tk.Entry(root, width=50)
+    name = tk.Entry(root, width=35)
+    process_label =tk.Label(root, text=state_no_process)
+    download_button = tk.Button(root, text='Download',command=lambda: click_download(), bg='white')
+    img = tk.PhotoImage()
+    save_button = tk.Button(root, text='Save', command=lambda: save(), bg='green')
+
+
+
+    title_label.pack()
+    input_url_label.pack()
+    url.pack()
+    process_label.pack()
+    download_button.pack()
+    name.pack()
+    name.config(state='disabled')
+
+    save_button.pack()
+    save_button.config(state='disabled')
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    run()
