7bd30d405dea14eaf3a57f4368206c7ca20155b3
Hakan <hakan@mbp18.com>
2023-11-02 23:35:34 -0500

    POC: First packageable version

--- app.py
+++ app.py
@@ -1,7 +1,63 @@
+import yt_dlp as DL
 import tkinter as tk
 from tkinter import filedialog
-from app.domain.services.video_service import download, get_info_without_download
 from pathvalidate import sanitize_filename
+
+
+def download(url:str, outname:str) -> str:
+    if(url == None):
+        print('Insert a valid url')
+        return
+    full_outname =  outname
+    ydl_opts = {
+        'format': 'best[height=720]',
+        'outtmpl': full_outname,
+        'nooverwrites': True,
+        'no_warnings': False,
+        'ignoreerrors': True,
+    }
+
+    result = 0
+
+    with DL.YoutubeDL(ydl_opts) as ydl:
+        r = ydl.download([url])
+        if(r == 1):
+            result = 1
+
+    if (result == 1):  # An error ocurred while downloading, it doesn't throw errors
+        print(
+            'Unable to download with 720p resolution. Trying its default best resolution.')
+        result = 0  # Restarting error indicator to its original value
+        ydl_opts['format'] = 'best'
+        with DL.YoutubeDL(ydl_opts) as ydl:
+            r = ydl.download([url])
+            if(r == 1):
+                result = 1
+
+    if (r == 1):
+        raise Exception('There was a problem while downloading')
+
+    return full_outname
+
+def get_info_without_download(url: str):
+    if(url == None):
+        print('Insert a url.')
+        return
+
+    ydl_opts = {
+
+    }
+    with DL.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return info
+    
+
+def get_formats(url:str):
+    formats = []
+    raw_formats = get_info_without_download(url)['formats']
+    for raw_format in raw_formats:
+        formats.append(raw_format['format_id'])
+    return formats
 
 def run() :
 
@@ -100,3 +156,9 @@ def run() :
 
 if __name__ == "__main__":
     run()
+
+
+
+
+    
+
---
+++ vidaun.spec
@@ -0,0 +1,43 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['app.py'],
+    pathex=['/Users/hakan/Documents/code/vidaun/app-python/venv/lib/python3.11/site-packages'],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='vidaun',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)
+app = BUNDLE(
+    exe,
+    name='vidaun.app',
+    icon=None,
+    bundle_identifier=None,
+)
