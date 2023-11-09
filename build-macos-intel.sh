13c792c6f51642a4843f81eb194b88c5298303ae
Hakan <hakan@mbp18.com>
2023-11-09 16:32:52 -0500

    Update README and macos build script

--- build-macos-intel.sh
+++ build-macos-intel.sh
@@ -5,6 +5,6 @@ pyinstaller vidaun.spec
 
 rm -R releases
 mkdir releases
-cp -R dist/vidaun.app releases/vidaun_macos_intel.app
-hdiutil create -volname "Vidaun" -srcfolder releases/vidaun_macos_intel.app  -ov -format UDZO releases/vidaum_macos_intel.dmg
-rm -R releases/vidaun_macos_intel.app
+cp -R dist/vidaun.app releases/Vidaun.app
+hdiutil create -volname "Vidaun" -srcfolder releases/Vidaun.app  -ov -format UDZO releases/vidaum_macos_intel.dmg
+rm -R releases/Vidaun.app
